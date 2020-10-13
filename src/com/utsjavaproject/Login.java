53f8d29e509af419340dc3b6fd0a861ee5fadee6
diazr <diaz.ridzky@gmail.com>
2020-10-13 20:43:18 +0700

    Initial Commit

---
+++ src/com/utsjavaproject/Login.java
@@ -0,0 +1,40 @@
+package com.utsjavaproject;
+
+public class Login {
+    private String Username;
+    private String Password;
+    private String InputUser;
+    private String InputPass;
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setInputUser(String InputUser) {
+        this.InputUser = InputUser;
+    }
+
+    public void setInputPass(String InputPass) {
+        this.InputPass = InputPass;
+    }
+
+    public String getInputUser() {
+        return InputUser;
+    }
+
+    public String getInputPass() {
+        return InputPass;
+    }
+}
--- src/com/utsjavaproject/Main.java
+++ src/com/utsjavaproject/Main.java
@@ -1,8 +1,60 @@
 package com.utsjavaproject;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I love Java cuy");
+        Login login = new Login();
+        Scanner keyboardInput = new Scanner(System.in);
+        int gagal = 0;
+
+        System.out.print("Selamat datang di Website ini, silahkan Sign Up jika ingin melihat berita terbaru \n");
+        System.out.print("\n");
+
+        System.out.print("Masukkan Username : ");
+        login.setUsername(keyboardInput.nextLine());
+        System.out.print("Masukkan Password : ");
+        login.setPassword(keyboardInput.nextLine());
+        System.out.print("Proses Sign Up selesai, silahkan login \n");
+
+        for(int i = 1; i <= 3; i++) {
+            System.out.print("Masukkan Username: ");
+            login.setInputUser(keyboardInput.nextLine());
+            System.out.print("Masukkan Password : ");
+            login.setInputPass(keyboardInput.nextLine());
+
+            boolean isEqualUser  = login.getUsername().equals(login.getInputUser());
+            boolean isEqualPass	 = login.getPassword().equals(login.getInputPass());
+            boolean notEqualUser = !login.getUsername().equals(login.getInputUser());
+            boolean notEqualPass = !login.getPassword().equals(login.getInputPass());
+
+            if(isEqualUser && isEqualPass) {
+                System.out.println("Login Sukses");
+                break;
+            }
+            else if(notEqualUser && notEqualPass) {
+                System.out.println("Login Gagal, silahkan coba lagi");
+                gagal++;
+                if(gagal > 3) {
+                    System.out.println("Silahkan contact administrator");
+                }
+            }
+            else if(isEqualUser && notEqualPass) {
+                System.out.println("Login Gagal, silahkan coba lagi");
+                gagal++;
+                if(gagal > 3) {
+                    System.out.println("Silahkan contact administrator");
+                }
+            }
+            else if(notEqualUser && isEqualPass) {
+                System.out.println("Login Gagal, silahkan coba lagi");
+                gagal++;
+                if(gagal > 3) {
+                    System.out.println("Silahkan contact administrator");
+                }
+            }
+
+        }
     }
 }
