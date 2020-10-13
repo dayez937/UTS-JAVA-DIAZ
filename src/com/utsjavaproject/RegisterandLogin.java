8c8e76012d05bff7fd7fa1d4ec89dc77aabac9b8
diazr <diaz.ridzky@gmail.com>
2020-10-13 20:44:29 +0700

    Initial Commit

--- src/com/utsjavaproject/RegisterandLogin.java
+++ src/com/utsjavaproject/RegisterandLogin.java
@@ -2,7 +2,7 @@ package com.utsjavaproject;
 
 import java.util.Scanner;
 
-public class Main {
+public class RegisterandLogin {
 
     public static void main(String[] args) {
         Login login = new Login();
